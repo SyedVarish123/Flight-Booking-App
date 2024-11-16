7d01aaa6bef6a10341db5f1047b1fad1defc53c1
SyedVarish123 <146931465+SyedVarish123@users.noreply.github.com>
2024-11-16 13:07:47 +0530

    Add files via upload

---
+++ member.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab1;
+
+/**
+ *
+ * @author Marc
+ */
+public class Member extends Passenger {
+
+    private int yearsOfMembership;
+
+    public Member(int yom, int a, String n) {
+        super(a, n);
+        yearsOfMembership = yom;
+
+    }
+
+    @Override
+    public double applyDiscount(double p) {
+        if (yearsOfMembership > 5) {
+            p = p / 2;
+            return p;
+        } else if (yearsOfMembership <= 5 && yearsOfMembership > 1) {
+            p = (p *9)/10;
+            return p;
+        }
+        return p;
+
+    }
+
+}
